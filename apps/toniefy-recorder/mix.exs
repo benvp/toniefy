249c1c7ec2f77b820183f53fb50f3d749f7c820c
Benjamin von Polheim <ben@vp.solutions>
2021-06-23 09:59:35 +0200

    fix: update chromedriver

--- apps/toniefy-recorder/mix.exs
+++ apps/toniefy-recorder/mix.exs
@@ -4,7 +4,7 @@ defmodule ToniexRecorder.MixProject do
   def project do
     [
       app: :toniex_recorder,
-      version: "1.0.2",
+      version: "1.0.3",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
