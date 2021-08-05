c5c82c3ec98afeabd8b743bedb58f7562f69a7eb
Benjamin von Polheim <ben@vp.solutions>
2021-08-05 13:35:16 +0200

    🔖 release v1.2.2

--- apps/toniefy/mix.exs
+++ apps/toniefy/mix.exs
@@ -4,7 +4,7 @@ defmodule Toniex.MixProject do
   def project do
     [
       app: :toniex,
-      version: "1.2.1",
+      version: "1.2.2",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
