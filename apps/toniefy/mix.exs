6fc2125b9ff5f2c53c9d4eff95eab94091079736
Benjamin von Polheim <ben@vp.solutions>
2021-11-21 21:25:28 +0100

    bump to 1.3.0

--- apps/toniefy/mix.exs
+++ apps/toniefy/mix.exs
@@ -4,7 +4,7 @@ defmodule Toniex.MixProject do
   def project do
     [
       app: :toniex,
-      version: "1.2.2",
+      version: "1.3",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
