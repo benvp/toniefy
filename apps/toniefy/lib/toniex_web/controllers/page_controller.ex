8857eb54c060a8b4c740f9eeccff933bccb65329
Benjamin von Polheim <ben@vp.solutions>
2021-11-21 19:47:18 +0100

    upgrade deps and cleanup unused pages (#1)

--- apps/toniefy/lib/toniex_web/controllers/page_controller.ex
+++ apps/toniefy/lib/toniex_web/controllers/page_controller.ex
@@ -1,15 +1,3 @@
 defmodule ToniexWeb.PageController do
   use ToniexWeb, :controller
-
-  def privacy(conn, _params) do
-    render(conn, "privacy.html")
-  end
-
-  def donate(conn, _params) do
-    render(conn, "donate.html")
-  end
-
-  def donate_success(conn, _params) do
-    render(conn, "donate_success.html")
-  end
 end
--- apps/toniefy/lib/toniex_web/live/page_live.ex
+++ apps/toniefy/lib/toniex_web/live/page_live.ex
@@ -1,16 +1,9 @@
 defmodule ToniexWeb.PageLive do
   use ToniexWeb, :live_view
 
-  alias Toniex.Waitlist
-
   @impl true
   def mount(_params, session, socket) do
-    socket =
-      assign_defaults(session, socket)
-      |> assign(
-        message: nil,
-        waitlist_changeset: Waitlist.change_entry(%Waitlist.Entry{})
-      )
+    socket = assign_defaults(session, socket)
 
     if socket.assigns.current_user do
       socket = push_redirect(socket, to: Routes.library_index_path(socket, :index))
@@ -21,21 +14,9 @@ defmodule ToniexWeb.PageLive do
   end
 
   @impl true
-  def handle_params(_params, uri, socket) do
-    {:noreply, assign(socket, uri: URI.parse(uri))}
-  end
-
-  @impl true
-  def handle_event("join_waitlist", %{"entry" => %{"email" => email}}, socket) do
-    case Waitlist.join(email) do
-      {:ok, _entry} ->
-        {:noreply,
-         assign(socket,
-           message: "Prima. Wir kontaktieren dich, sobald dein Zugang freigeschaltet wurde."
-         )}
-
-      {:error, changeset} ->
-        {:noreply, assign(socket, waitlist_changeset: changeset)}
-    end
+  def render(assigns) do
+    ~L"""
+    <div></div>
+    """
   end
 end
